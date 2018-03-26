b9c1c84a53b8070678905921dcb491d65331f43f
Anderson Borges <andersonvaleriano.borges@gmail.com>
2018-03-26 09:11:45 -0300

    add require autoload

--- ictool.php
+++ ictool.php
@@ -2,4 +2,16 @@
 <?php
 
 define('ENV', 'dev');
+
+$basePath = getcwd();
+
+if (file_exists("$basePath/vendor/autoload.php")) {
+    require_once "$basePath/vendor/autoload.php";
+} elseif (\Phar::running()) {
+    require_once __DIR__ . '/vendor/autoload.php';
+} else {
+    error_log('Error: I cannot find the autoloader of the application.' . PHP_EOL);
+    exit(2);
+}
+
 require __DIR__ . '/src/cli.php';
